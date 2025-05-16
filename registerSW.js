6bb616492d8dc7d3f27a4639ebeabcf53d5e7eac
smaftoul <smaftoul@users.noreply.github.com>
2025-05-16 15:07:49 +0000

    deploy: 24021b1a98a655313b44aecbece9660dfe5fa648

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://smaftoul.github.io/gbif-explorer.github.io/sw.js', { scope: 'https://smaftoul.github.io/gbif-explorer.github.io/' })})}
